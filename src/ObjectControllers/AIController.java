0095235e05556ff4e73fc85fe95ac374e20712e7
jack <jack@jack.(none)>
2013-10-15 00:14:34 -0400

    added package for controlling objects

---
+++ src/ObjectControllers/AIController.java
@@ -0,0 +1,22 @@
+package ObjectControllers;
+
+import Engine.IGameEngine;
+import GameObjects.PhysicsObject;
+
+// this will control objects based on what is going on in the game.
+// ... and will be probably rly basic
+public class AIController extends ObjectController
+{
+
+	public AIController(PhysicsObject obj, IGameEngine eng) 
+	{
+		super(obj, eng);
+	}
+
+	@Override
+	public void controlObject() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/ObjectControllers/ObjectController.java
@@ -0,0 +1,19 @@
+package ObjectControllers;
+
+import Engine.IGameEngine;
+import GameObjects.PhysicsObject;
+
+// an abstract class for controlling PhysicsObjects
+public abstract class ObjectController 
+{
+	public ObjectController(PhysicsObject obj, IGameEngine eng)
+	{
+		this.obj = obj;
+	}
+	
+	// generally this will controll objects by applying external forces to them
+	public abstract void controlObject();
+	
+	PhysicsObject obj; 
+	IGameEngine eng; 
+}
