713c28b8bbdffa0b099e21857509e19c5fa14caf
jack <jack@jack.(none)>
2013-10-16 11:08:30 -0400

    fixed some stuff

--- src/Actions/PhysicsObjectController.java
+++ src/Actions/PhysicsObjectController.java
@@ -4,7 +4,7 @@ import Engine.IGameEngine;
 import GameObjects.GameObject;
 
 // controls PhysicsObjects by applying forces to them
-public abstract class PhysicsObjectController extends Physics
+public abstract class PhysicsObjectController extends CollidablePhysics
 {
 	public PhysicsObjectController(GameObject obj, IGameEngine eng, float deltaT) 
 	{
