f860ee1264399494760b866c6e116587f2c6ea5c
jack <jack@jack.(none)>
2013-11-13 16:42:59 -0500

    created an easy way to verify collisions are being calculated correctly

--- src/GameStates/CollisionTesting.java
+++ src/GameStates/CollisionTesting.java
@@ -1,5 +1,7 @@
 package GameStates;
 
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector4f;
 
 import Actions.MouseTracker;
@@ -40,55 +42,79 @@ public class CollisionTesting extends EventListenerState implements IGameState
 	@Override
 	public void Update() {
 		super.Update();
-		universe.update((float)1/game.GetTickFrequency());
-		game.SetWindowTitle("" + game.GetFrameRate());
+//		universe.update((float)1/game.GetTickFrequency());
+//		game.SetWindowTitle("" + game.GetFrameRate());
 	}
 
 	@Override
 	public void Draw(float delta) {
 		gfx.ClearScreen();
-		universe.draw((float)(delta/game.GetTickFrequency()));
-		if(obj1.getBoundingBox().bl != null)
-		{
-			gfx.DrawRectangle(
-					obj1.getBoundingBox().bl.x, 
-					obj1.getBoundingBox().bl.y, 
-					obj1.getBoundingBox().tr.x, 
-					obj1.getBoundingBox().tr.y);
-
-			gfx.DrawRectangle(
-					obj2.getBoundingBox().bl.x, 
-					obj2.getBoundingBox().bl.y, 
-					obj2.getBoundingBox().tr.x, 
-					obj2.getBoundingBox().tr.y);
-		}
-		
+//		universe.draw((float)(delta/game.GetTickFrequency()));
+//		if(obj1.getBoundingBox().bl != null)
+//		{
+//			gfx.DrawRectangle(
+//					obj1.getBoundingBox().bl.x, 
+//					obj1.getBoundingBox().bl.y, 
+//					obj1.getBoundingBox().tr.x, 
+//					obj1.getBoundingBox().tr.y);
+//
+//			gfx.DrawRectangle(
+//					obj2.getBoundingBox().bl.x, 
+//					obj2.getBoundingBox().bl.y, 
+//					obj2.getBoundingBox().tr.x, 
+//					obj2.getBoundingBox().tr.y);
+//		}
+//		
 	}
 	
 	private void buildUniverse()
 	{
-		universe = new GameObject();
-		universe.scale(.2f, .2f);
-		obj1 = new GameObject();
-		obj1.setBoundingBox(new AABB(.5f, .5f)); 
-		obj1.setDrawingInterface(new SimpleDraw(gfx.LoadTexture("image.bmp", 0))); 
-		obj1.setCollidable(true); // makes it so other objects can collide with obj1. not nessessary for this example 
-								  // but for more complex scenes it will allow other collidablePhysics objects to collide with obj1
-		PCControl pc = new PCControl(obj1, universe, game);
-		this.addMouseEventListener(pc);
-		this.addKeyEventListener(pc);
-		obj1.addAction(pc); // PCControl extends CollidablePhysics.
-		
-		obj2 = new GameObject();
-		obj2.setBoundingBox(new AABB(.5f, .5f)); // set objects bounds. units in global coordinates.
-		obj2.translate(3, 0);
-		obj2.setDrawingInterface(new SimpleDraw(gfx.LoadTexture("image.bmp", 0)));
-		obj2.setCollidable(true); // makes it so other objects can collide with obj2.
-		
-		universe.addChild(obj2);
 		
 		
-		universe.addChild(obj1);
+		
+		AABB bounds1 = new AABB(2,2);
+		AABB bounds2 = new AABB(2,2);
+		Matrix4f transform1 = new Matrix4f();
+		Matrix4f transform2 = new Matrix4f();
+		
+		
+		transform1.translate(new Vector2f(0,0)); 	// move bounds1 0,0 units
+		transform2.translate(new Vector2f(1,0));	// move bounds2 1,0 units
+		bounds1.transform(transform1); 				//
+		bounds2.transform(transform2);				//
+
+
+		System.out.printf("bounds1 tr: %s\n", bounds1.tr);
+		System.out.printf("bounds1 bl: %s\n", bounds1.bl);
+		System.out.printf("bounds2 tr: %s\n", bounds2.tr);
+		System.out.printf("bounds2 bl: %s\n", bounds2.bl);
+		
+		
+		Vector2f outof = bounds1.solveCollision(bounds2);
+		System.out.printf("outof %s\n", outof);
+		
+//		universe = new GameObject();
+//		universe.scale(.2f, .2f);
+//		obj1 = new GameObject();
+//		obj1.setBoundingBox(new AABB(.5f, .5f)); 
+//		obj1.setDrawingInterface(new SimpleDraw(gfx.LoadTexture("image.bmp", 0))); 
+//		obj1.setCollidable(true); // makes it so other objects can collide with obj1. not nessessary for this example 
+//								  // but for more complex scenes it will allow other collidablePhysics objects to collide with obj1
+//		PCControl pc = new PCControl(obj1, universe, game);
+//		this.addMouseEventListener(pc);
+//		this.addKeyEventListener(pc);
+//		obj1.addAction(pc); // PCControl extends CollidablePhysics.
+//		
+//		obj2 = new GameObject();
+//		obj2.setBoundingBox(new AABB(.5f, .5f)); // set objects bounds. units in global coordinates.
+//		obj2.translate(3, 0);
+//		obj2.setDrawingInterface(new SimpleDraw(gfx.LoadTexture("image.bmp", 0)));
+//		obj2.setCollidable(true); // makes it so other objects can collide with obj2.
+//		
+//		universe.addChild(obj2);
+//		
+//		
+//		universe.addChild(obj1);
 	}
 	
 	GameObject universe, obj1, obj1child, obj2;
